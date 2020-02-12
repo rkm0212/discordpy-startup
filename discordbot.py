ed55a84ee9342b5547c5224ec57b7fc70dee79d8
rkm0212 <60937088+rkm0212@users.noreply.github.com>
2020-02-13 02:33:57 +0900

    teest

--- discordbot.py
+++ discordbot.py
@@ -1,27 +1,22 @@
-from discord.ext import commands
-import os
-import traceback
+#coding:UTF-8
+import discord
+from discord.ext import tasks
 
-bot = commands.Bot(command_prefix='/')
-token = os.environ['DISCORD_BOT_TOKEN']
+# 自分のBotのアクセストークンに置き換えてください
+TOKEN = 'Njc2NzcwNjY3NjUxMzk5NzEx.XkQwCA.6GXGif45hWZ7PA0HR9hzaphsyRk'
+CHANNEL_ID = 676772811595055136
 
+# 接続に必要なオブジェクトを生成
+client = discord.Client()
 
-@bot.event
-async def on_command_error(ctx, error):
-    orig_error = getattr(error, "original", error)
-    error_msg = ''.join(traceback.TracebackException.from_exception(orig_error).format())
-    await ctx.send(error_msg)
-
-@client.event
-async def on_ready():
-    CHANNEL_ID = 676772811595055136 # 任意のチャンネルID(int)
+# 60秒に一回ループ
+@tasks.loop(seconds=5)
+async def loop():
     channel = client.get_channel(CHANNEL_ID)
-    await channel.send('おはよう！')
+    await channel.send('5秒たったよ')  
 
+#ループ処理実行
+loop.start()
 
-@bot.command()
-async def ping(ctx):
-    await ctx.send('norple')
-
-
-bot.run(token)
+# Botの起動とDiscordサーバーへの接続
+client.run(TOKEN)
